3e70cc11443fd56bae93d0792ffdbf17cf1660f2
purna 2203 <purnashri.r.r3895@gmail.com>
2025-06-09 16:44:18 +0530

    first commit

--- java.js
+++ java.js
@@ -1,3 +1,16 @@
-const choice=('rock','paper','scissor')
-            let computer=choices[math.floor(math.random()*3)]
-            console.log(computer);
+console.log('hello from script');
+ console.log(5==='5');
+ console.log(5=='5');
+ let i=0
+ do{
+    i++;
+    console.log(i);
+ }while(i<5)
+   function greet(name ,age){
+      return ` Hello ${name }  is ${ age } old`
+      }
+   console.log(greet('Varshine',18));
+   function add(a,b){
+      return (a+b)
+   }
+console.log(add(10,20))
